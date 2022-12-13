c5584e5444a2a361120390e5d8c7d9206c250a4b
Pranali <vanipranali@gmail.com>
2022-12-13 03:33:18 -0500

    Fixed json error, removed dead code and prints

--- VOCR/utils.py
+++ VOCR/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 from rectangle import Rectangle
 from classifier import Classifier
 
-EPSILON = 5
+EPSILON = 2
 HOUGH_CIRCLE_PARAMS = {"minDist":30, 
                         "param1":40, 
                         "param2":100, #smaller value-> more false circles
@@ -120,7 +120,6 @@ def _prune_rectangles(rectangles, text_rects, min_size, max_size):
         if not overlaps_with_text:
             small_rectangles.append(rect1)
     print("After pruning, there are {} rectangles".format(len(small_rectangles)))
-    print(small_rectangles)
     return small_rectangles
 
 def get_rects_for_image(img, width, height, text_rects, text_labels, validation=False):
@@ -139,7 +138,7 @@ def get_rects_for_image(img, width, height, text_rects, text_labels, validation=
         text_rectangles.append(text_rect)
 
     # scale image and convert to grayscale
-    min_dist_between = EPSILON
+    min_dist_between = max(EPSILON, (min(height, width) / 250))
     img = np.uint8(img)
     assert(np.prod(img.shape) == np.prod((height, width, 3)))
     img = np.array(img).reshape((height, width, 3))
@@ -158,7 +157,7 @@ def get_rects_for_image(img, width, height, text_rects, text_labels, validation=
         rect = Rectangle(14, NO_CONFIDENCE, *tup)
         rectangles.append(rect) # rectangle with no label and 0% confidence
         
-    total_image_size = np.prod(gray.shape)
+    total_image_size = width*height
 
     # Prune out large rectangles
     rectangles = _prune_rectangles(rectangles, text_rectangles, 0, MAX_PERCENT_OF_IMAGE*total_image_size/100)
@@ -179,8 +178,6 @@ def get_rects_for_image(img, width, height, text_rects, text_labels, validation=
                     if j in removed_idx:
                         continue
                     if _check_rectangle_overlap(expanding_rect, rect2, min_dist_between):
-                        print("expanding_rect", expanding_rect)
-                        print("rect2", rect2)
                         expanding_rect = _get_combined_rect(expanding_rect, rect2)
                         still_combining = True  
                         removed_idx.add(j)
@@ -201,30 +198,15 @@ def get_rects_for_image(img, width, height, text_rects, text_labels, validation=
                     assert (not _check_rectangle_overlap(rect1, rect2, min_dist_between)), "failed: " + str(rect1) + str(rect2)
 
     rect_tuples = [rect.get_values() for rect in final_rectangles]
-    print("rect_tuples", rect_tuples)
-    print("img width", width)
-    print("img height", height)
     labels = cf.classify_n(rect_tuples)
-    # labels = ["unknown" for tup in rect_tuples]
 
     for i, rect in enumerate(final_rectangles):
         rect.set_label(labels[i])
-        # rect.normalize(width, height)
 
-    # for rect in text_rectangles:
-    #     rect.normalize(width, height)
-
-    # final_dims = [rect.get_values() for rect in final_rectangles] + [rect.get_values() for rect in text_rectangles]
-    # final_labels = [rect.label[0] for rect in final_rectangles] + [rect.label[0] for rect in text_rectangles]
-    # final_dims.append((0, 0, width, height))
-    # final_labels.append("Outer Bounds Box")
 
     data = []
     for rect in final_rectangles:
-        data.append([*rect.get_values(), rect.label])
+        data.append([*rect.get_values(), int(rect.label)])
     data.append([0, 0, width, height, 14])
 
-    # print(final_dims)
-    # print(final_labels)
-    # return final_dims, final_labels
     return data
